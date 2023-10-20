0098d74907203c9cb5e372e4f2661d083b401b4f
jaypal1-7 <52853399+jaypal1-7@users.noreply.github.com>
2023-10-20 17:21:20 -0400

    Update HMM_Viterbi.py

--- Main/HMM_Viterbi.py
+++ Main/HMM_Viterbi.py
@@ -6,144 +6,141 @@
 
 import numpy as np
 
+class HMMViterbi:
+    """Hidden Markov Model (HMM) Viterbi class for sequence tagging."""
 
-class HMM_Viterbi(object):
-    #HMM Viterbi class holds initial, transition and Emmission probabality data
-    
-    def __init__(self,States=[],Observations=[], InitialProb = {},TransProb ={},EmissionProb={},TransMat=[]):
-        self.States = States
-        self.Observations=Observations
-        self.InitialProb=InitialProb
-        self.TransProb=TransProb
-        self.EmissionProb=EmissionProb
-        self.TransMat = TransMat
-        self.ForwardValues =[]
-        self.BackwardValues = []
-        self.ProbForward = []
-        self.PosteriorValues =[]
+    def __init__(self, states=[], observations=[], initial_probabilities={}, transition_probabilities={},
+                 emission_probabilities={}, transition_matrix=[]):
+        self.states = states
+        self.observations = observations
+        self.initial_probabilities = initial_probabilities
+        self.transition_probabilities = transition_probabilities
+        self.emission_probabilities = emission_probabilities
+        self.transition_matrix = transition_matrix
+        self.forward_values = []
+        self.backward_values = []
+        self.prob_forward = []
+        self.posterior_values = []
 
+    def set_parameters(self, encoded_text_list):
+        self.states = list(range(max(encoded_text_list)))
+        self.observations = list(range(max(encoded_text_list)))
+        self.initial_probabilities = self.calculate_initial_probabilities(encoded_text_list)
+        self.transition_matrix = self.calculate_transition_matrix(encoded_text_list)
+        self.transition_probabilities = self.convert_to_dict(self.transition_matrix)
+        self.emission_probabilities = self.convert_to_dict(self.transition_matrix.transpose(1, 0))
 
-    #Set Parameter Values 
-    def Set_Parameters(self,encodedTextList):
-        self.States = (list(range(max(encodedTextList))))
-        self.Observations = (list(range(max(encodedTextList))))
-        self.InitialProb = Initial_prob(encodedTextList)
-        self.TransMat = Trans_Mat(encodedTextList)
-        Transmat = Trans_Mat(encodedTextList)
-        self.TransProb = Convert_to_dict(Transmat)
-        self.EmissionProb = Convert_to_dict(Transmat.transpose(1,0))
-        
-    #Initial Probability
-    def Initial_prob(encodedTextList):
-        dictnry = {}
-        tranDist = sorted(set(encodedTextList))
-        tranProb = []
-        length= len(encodedTextList)
-        
-        for x in tranDist:
-            tranProb.append(len(np.where(encodedTextList==x)[0])/length)
-        arr = np.array(tranProb)
-        for i,prob in zip(list(range(len(arr))),arr):
-            dictnry[i]=prob
-        return dictnry
+    def calculate_initial_probabilities(self, encoded_text_list):
+        initial_probabilities = {}
+        unique_states = sorted(set(encoded_text_list))
+        transition_probabilities = []
+        length = len(encoded_text_list)
 
-    
-    #Transition Matrix calculation for m states
-    def Trans_Mat(encodedTextList):
-        m_states = 1+max(encodedTextList)
-        TranMatrix = np.zeros(shape=(m_states,m_states))
-        
-        for i in range(0,m_states):
-            EL = list(np.where(encodedTextList==i)[0])
-            for el in EL:
-                if(el +1 != len(encodedTextList)):
-                    TranMatrix[i,encodedTextList[el+1]] += 1/len(EL)
-        TranMatrix[TranMatrix!=0]= TranMatrix[TranMatrix!=0] + (1/m_states)
-        TranMatrix[TranMatrix==0]=1/m_states
-        return TranMatrix
+        for x in unique_states:
+            transition_probabilities.append(len(np.where(encoded_text_list == x)[0]) / length)
+        probabilities_array = np.array(transition_probabilities)
 
+        for i, prob in zip(list(range(len(probabilities_array)), probabilities_array):
+            initial_probabilities[i] = prob
+        return initial_probabilities
 
-    def Convert_to_dict(TranMatrix):
-        dict1 = {}
-        for i,row in zip(list(range(np.size(matrix,axis=0))),matrix):
+    def calculate_transition_matrix(self, encoded_text_list):
+        num_states = 1 + max(encoded_text_list)
+        transition_matrix = np.zeros(shape=(num_states, num_states))
+
+        for i in range(0, num_states):
+            state_indices = list(np.where(encoded_text_list == i)[0])
+            for index in state_indices:
+                if (index + 1 != len(encoded_text_list)):
+                    transition_matrix[i, encoded_text_list[index + 1]] += 1 / len(state_indices)
+
+        transition_matrix[transition_matrix != 0] = transition_matrix[transition_matrix != 0] + (1 / num_states)
+        transition_matrix[transition_matrix == 0] = 1 / num_states
+
+        return transition_matrix
+
+    def convert_to_dict(self, matrix):
+        dictionary = {}
+        for i, row in zip(list(range(np.size(matrix, axis=0)), matrix):
             temp = {}
-            for j,val in zip(list(range(np.size(row,axis=0))),row):
-                temp[j]=val
-            dict1[i]=temp
-        return dict1
-    
+            for j, val in zip(list(range(np.size(row, axis=0)), row):
+                temp[j] = val
+            dictionary[i] = temp
+        return dictionary
 
-
-    #Viterbi Algorithm Implementation
-    def Viterbi_Algo(TranMatrix, EmissMat, observations):
-        num_obs = len(observations)
-        num_states = TranMatrix.shape[0]
+    def viterbi_algorithm(self, transition_matrix, emission_matrix, observations):
+        num_observations = len(observations)
+        num_states = transition_matrix.shape[0]
         log_probs = np.zeros(num_states)
-        paths = np.zeros( (num_states, num_obs+1 ))
+        paths = np.zeros((num_states, num_observations + 1))
         paths[:, 0] = np.arange(num_states)
+
         for obs_ind, obs_val in enumerate(observations):
             for state_ind in range(num_states):
-                val = 0
-                if obs_val< np.size(EmissMat,1):
-                    val = np.log(EmissMat[state_ind, obs_val])
-                temp_probs = log_probs +                               val +                              np.log(TranMatrix[:, state_ind])
+                value = 0
+                if obs_val < np.size(emission_matrix, 1):
+                    value = np.log(emission_matrix[state_ind, obs_val])
+                temp_probs = log_probs + value + np.log(transition_matrix[:, state_ind])
                 best_temp_ind = np.argmax(temp_probs)
-                paths[state_ind,:] = paths[best_temp_ind,:]
-                paths[state_ind,(obs_ind+1)] = state_ind
+                paths[state_ind, :] = paths[best_temp_ind, :]
+                paths[state_ind, (obs_ind + 1)] = state_ind
                 log_probs[state_ind] = temp_probs[best_temp_ind]
+
         best_path_ind = np.argmax(log_probs)
-    
+
         return (paths[best_path_ind], log_probs[best_path_ind])
 
-
-    def Posterior(self):
+    def calculate_posterior(self):
         posterior = []
-        for i in range(len(self.Observations)):
-            posterior.append({st: self.ForwardValues[i][st] * self.BackwardValues[i][st] / self.ProbForward for st in self.States})
+        for i in range(len(self.observations)):
+            posterior.append({
+                st: self.forward_values[i][st] * self.backward_values[i][st] / self.prob_forward
+                for st in self.states
+            })
 
         return posterior
 
-    #Forward Algorithm
-    def ForwardAlgo(self, EndState):
-        fwd = []
+    def forward_algorithm(self, end_state):
+        forward_values = []
         f_prev = {}
-        for i, observation_i in enumerate(self.Observations):
+        for i, observation_i in enumerate(self.observations):
             f_curr = {}
-            for st in self.States:
+            for st in self.states:
                 if i == 0:
-                    prev_f_sum = self.InitialProb[st]
+                    prev_f_sum = self.initial_probabilities[st]
                 else:
-                    prev_f_sum = sum(f_prev[k]*self.TransProb[k][st] for k in self.States)
+                    prev_f_sum = sum(f_prev[k] * self.transition_probabilities[k][st] for k in self.states)
 
-                f_curr[st] = self.EmissionProb[st][observation_i] * prev_f_sum
+                f_curr[st] = self.emission_probabilities[st][observation_i] * prev_f_sum
 
-            fwd.append(f_curr)
+            forward_values.append(f_curr)
             f_prev = f_curr
 
-        p_fwd = sum(f_curr[k] * self.TransProb[k][EndState] for k in self.States)
+        prob_forward = sum(f_curr[k] * self.transition_probabilities[k][end_state] for k in self.states)
 
+        return forward_values, prob_forward
 
-        return fwd,p_fwd
-
-    #Backward Algorithm
-    def BackwardAlgo(self, EndState = 'PERIOD'):
-        backw = []
+    def backward_algorithm(self, end_state='PERIOD'):
+        backward_values = []
         b_prev = {}
-        for i, observation_i_plus in enumerate(reversed(self.Observations[1:]+[None,])):
+        for i, observation_i_plus in enumerate(reversed(self.observations[1:] + [None, ])):
             b_curr = {}
-            for st in self.States:
+            for st in self.states:
                 if i == 0:
-                    # base case for backward part
-                    b_curr[st] = self.TransProb[st][EndState]
+                    b_curr[st] = self.transition_probabilities[st][end_state]
                 else:
-                    b_curr[st] = sum(self.TransProb[st][l] * self.EmissionProb[l][observation_i_plus] * b_prev[l] for l in self.States)
+                    b_curr[st] = sum(
+                        self.transition_probabilities[st][l] * self.emission_probabilities[l][observation_i_plus] * b_prev[l]
+                        for l in self.states
+                    )
 
-            backw.insert(0,b_curr)
+            backward_values.insert(0, b_curr)
             b_prev = b_curr
 
-        p_bkw = sum(self.InitialProb[l] * self.EmissionProb[l][self.Observations[0]] * b_curr[l] for l in self.States)
+        p_bkw = sum(
+            self.initial_probabilities[l] * self.emission_probabilities[l][self.observations[0]] * b_curr[l]
+            for l in self.states
+        )
 
-        return backw
-
-    
+        return backward_value
 
